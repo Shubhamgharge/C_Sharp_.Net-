6e6a7b04313e91210c3abef65ae2f9de1d080bae
Shubham Gharge <159037288+Shubhamgharge@users.noreply.github.com>
2025-06-15 23:48:10 +0530

    Add files via upload

--- 00 Cansole_programs/Program.cs
+++ 00 Cansole_programs/Program.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Subtraction
+namespace Diplay_first_number_In_Second_number
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int iNo1 = 0, iNo2 = 0, iAns = 0;
+            int iNum1 = 0, iNum2 = 0, i = 0;
 
             Console.Write("Enter 1st number : ");
-            iNo1 = Convert.ToInt32(Console.ReadLine());
+            iNum1 = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter 2nd number : ");
-            iNo2 = Convert.ToInt32(Console.ReadLine());
+            iNum2 = Convert.ToInt32(Console.ReadLine());
 
-            iAns = iNo1 - iNo2;
-
-            Console.Write("\n Subtraction of " + iNo1 + " & " + iNo2 + " is " + iAns + ".");
+            for(i =1; i<=iNum2 ; i++)
+            {
+                Console.Write(+iNum1+" ");
+            }
 
             Console.ReadKey();
-
         }
     }
 }
