1936a7b4c1cb4fd56d55f1dee4d3651c9d0c07ee
Shubham Gharge <159037288+Shubhamgharge@users.noreply.github.com>
2025-06-16 23:29:53 +0530

    Add files via upload

---
+++ Cansole_Programs/Diplay_first_number_In_Second_number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplay_first_number_In_Second_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum1 = 0, iNum2 = 0, i = 0;
+
+            Console.Write("Enter 1st number : ");
+            iNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 2nd number : ");
+            iNum2 = Convert.ToInt32(Console.ReadLine());
+
+            for(i =1; i<=iNum2 ; i++)
+            {
+                Console.Write(+iNum1+" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
