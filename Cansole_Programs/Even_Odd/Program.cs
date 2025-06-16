5f9f3db7a2d3ec994273a4e9ef70a876c4bfe72b
Shubham Gharge <159037288+Shubhamgharge@users.noreply.github.com>
2025-06-16 23:30:30 +0530

    Add files via upload

---
+++ Cansole_Programs/Even_Odd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum = 0;
+
+            Console.Write("Enter a number : ");
+            iNum = Convert.ToInt32(Console.ReadLine());
+
+            if(iNum == 0)
+            {
+                return;
+            }
+            else if(iNum % 2 == 0)
+            {
+                Console.Write(+iNum + " is Enen number .");
+            }
+            else
+            {
+                Console.Write(+iNum + " is Odd number .");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
